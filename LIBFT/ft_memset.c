e1493557c24b4a4704657abb0b7e05e8a84f0744
João Gomes <gomesjoao08@hotmail.com>
2024-04-10 22:32:26 +0100

    Add files via upload

---
+++ LIBFT/ft_memset.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gomes <gomes@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/10 16:13:58 by gomes             #+#    #+#             */
+/*   Updated: 2024/04/10 16:31:39 by gomes            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "stdio.h"
+
+void *ft_memset(void *s, int c, size_t n)
+{
+    char    *str;
+    size_t i;
+
+    str = (char *)s
+    i = 0;
+    while (i < n)
+    {
+        str[i] = (unsigned char)c;
+        i++;
+    }
+    return (s);
+}
+
+/*
+writes n bytes of value c to the string s. return first argument.
+*/
