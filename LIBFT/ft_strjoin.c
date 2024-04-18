0a8c4a907708a869e3a8a0aa34d656f8fcc42f17
Gomeskk <gomesjoao08@hotmail.com>
2024-04-18 17:07:33 +0100

    strjoin working

--- LIBFT/ft_strjoin.c
+++ LIBFT/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: joafaust <joafaust@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 15:41:43 by joafaust          #+#    #+#             */
-/*   Updated: 2024/04/18 16:44:06 by joafaust         ###   ########.fr       */
+/*   Updated: 2024/04/18 17:07:02 by joafaust         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,38 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		i;
 	int		a;
 
-	a = 0;
 	i = 0;
+	a = 0;
 	res = malloc(ft_strlen((char *)s1) + ft_strlen((char *)s2) + 1);
 	if (res == 0)
 		return (NULL);
-	while (s1[i])
-		res[a++] = s1[i++];
+	while (s1 && s1[i])
+		res[a++] = s1[i++];	
 	i = 0;
-	while (s2[i])
-		res[a] = s2[i];
+	while (s2 && s2[i])
+		res[a++] = s2[i++];
+
 	res[a] = 0;
 	return (res);
 }
+
+/*
+int main(void)
+{
+    char *s1 = "Hello, ";
+    char *s2 = "world!";
+    char *result;
+
+    result = ft_strjoin(s1, s2);
+    if (result == NULL)
+    {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+
+    printf("Joined string: %s\n", result);
+
+    free(result); // Don't forget to free allocated memory
+    return 0;
+}
+*/
